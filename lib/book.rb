c410c58fa1670f20c9d0c34f766b6dc1644717fc
Christopher <chrismbogo97@gmail.com>
2022-10-25 10:38:59 +0300

    Completed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
+require 'pry'
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+def initialize (title)
+    @title=title
+end
 
+def turn_page
+    puts "Flipping the page...wow, you read fast!"
+end
+end
 
+harry_potter=Book.new("Harry Potter")
+harry_potter.page_count="14000 words"
+# binding.pry
